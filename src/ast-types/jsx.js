0259ca89c0fc43bc89ff9fb3c5f2d9bf21c52385
Roman Liutikov <roman01la@romanliutikov.com>
2018-06-20 16:28:32 +0300

    add js/jsx ast types listing

---
+++ src/ast-types/jsx.js
@@ -0,0 +1,14 @@
+module.exports = [
+  "JSXAttribute",
+  "JSXClosingElement",
+  "JSXElement",
+  "JSXEmptyExpression",
+  "JSXExpressionContainer",
+  "JSXSpreadChild",
+  "JSXIdentifier",
+  "JSXMemberExpression",
+  "JSXNamespacedName",
+  "JSXOpeningElement",
+  "JSXSpreadAttribute",
+  "JSXText"
+];
