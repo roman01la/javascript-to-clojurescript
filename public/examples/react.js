2ec786aa821881fb69c234c397a78cfd2bcc7bfb
roman01la <roman01la@romanliutikov.com>
2019-08-16 23:01:56 +0300

    rewrite React example with hooks

--- public/examples/react.js
+++ public/examples/react.js
@@ -1,49 +1,34 @@
-function State(initial) {
-  const ctx = { st: initial, listeners: [] };
-  return {
-    update: next => {
-      ctx.st = Object.assign(ctx.st, next);
-      ctx.listeners.forEach(fn => fn(ctx.st));
-    },
-    deref: () => ctx.st,
-    listen: f => ctx.listeners.push(f)
-  };
-}
+const App = () => {
+  const [state, setState] = React.useState({ value: "", repos: [] });
 
-const state = new State({ value: "", repos: [] });
+  function handleSubmit(e, uname) {
+    e.preventDefault();
+    fetch("https://api.github.com/users/" + uname + "/repos")
+      .then(res => res.json())
+      .then(json => {
+        setState({ ...state, repos: json });
+      })
+      .catch(err => console.log(err));
+  }
 
-function handleSubmit(e, uname) {
-  console.log(uname);
-  e.preventDefault();
-  fetch("https://api.github.com/users/" + uname + "/repos")
-    .then(res => res.json())
-    .then(json => {
-      console.log(JSON.stringify(json[0]));
-      state.update({ repos: json });
-    })
-    .catch(err => console.log(err));
-}
-
-const App = st =>
-  html(
+  return html(
     <div>
-      <form onSubmit={e => handleSubmit(e, st.value)}>
+      <form onSubmit={e => handleSubmit(e, state.value)}>
         <input
           placeholder="GitHub user name"
-          value={st.value}
+          value={state.value}
           onChange={e => {
-            state.update({ value: e.target.value });
+            setState({ ...state, value: e.target.value });
           }}
         />
         <button>Fetch</button>
       </form>
-      {"repos fetched: " + st.repos.length}
+      {"repos fetched: " + state.repos.length}
     </div>
   );
+};
 
-const render = st =>
-  ReactDOM.render(App(st), document.getElementById("react-root"));
-
-render(state.deref());
-
-state.listen(render);
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById("react-root")
+);
