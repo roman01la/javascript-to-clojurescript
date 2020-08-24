0aea5f4c90989cc68b6f09acd5099a7aeba2e71c
roman01la <roman01la@romanliutikov.com>
2020-08-24 15:00:40 +0300

    rm unused

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-// const fs = require("fs");
-// const path = require("path");
 const { parse } = require("babylon");
 const zprint = require("zprint-clj");
 
 const generate = require("./cljs-gen");
 const transformAST = require("./js2cljs");
 const addSyntaxSugar = require("./syntax-builder");
-
-// const jscode = fs.readFileSync(path.join(__dirname, "test.js"), "utf8");
-
-// const ast = transformAST(parse(jscode, { plugins: ["jsx"] }));
-
-// console.log(JSON.stringify(ast, null, 2));
-// const genast = generate(ast);
-// const code = zprint(genast, "", { isHangEnabled: false });
-
-// console.log(code);
 
 const toLispAST = code =>
   transformAST(
